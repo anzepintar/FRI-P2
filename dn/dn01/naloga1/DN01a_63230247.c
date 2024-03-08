718c2049a3f1ef7eb73557cdd8e3df17ea3ef8d2
Anže Pintar <anze@anzepintar.com>
2024-03-08 08:16:09 +0100

    posodobljena naloga2

--- dn/dn01/naloga1/DN01a_63230247.c
+++ dn/dn01/naloga1/DN01a_63230247.c
@@ -22,58 +22,37 @@ void izpis(int n) {
         izpis(n / 10);
     putchar(n % 10 + '0');
 }
-
-int main() {
-    int a;
-    int aPredznak = 1;
-    int aPotenca = 1;
+int dobiStevilo() {
+    int st = 0;
+    int predznak = 1;
+    int potenca = 1;
     int i = 0;
-    int st0a = 0;
+    int st0 = 0;
     while (0 == 0) {
         i++;
-        int n = getchar();
-        if (n == ' ') {
-            break;
-        }
-        if (n == '-')
-            aPredznak = -1;
-        else {
-            if (n == '0') {
-                st0a++;
-            } else {
-                st0a = 0;
-            }
-            a = a + (n - '0') * aPotenca;
-            aPotenca = aPotenca * 10;
-        }
-    }
-    a = obrni(a, st0a) * aPredznak;
-
-    int b;
-    int bPredznak = 1;
-    int bPotenca = 1;
-    int j = 0;
-    int st0b = 0;
-    while (0 == 0) {
-        j++;
         int n = getchar();
         if (n == ' ' || n == '\n') {
             break;
         }
         if (n == '-') {
-            bPredznak = -1;
+            predznak = -1;
         } else {
             if (n == '0') {
-                st0b++;
+                st0++;
             } else {
-                st0b = 0;
+                st0 = 0;
             }
-            b = b + (n - '0') * bPotenca;
-            bPotenca = bPotenca * 10;
+            st = st + (n - '0') * potenca;
+            potenca = potenca * 10;
         }
     }
-    b = obrni(b, st0b) * bPredznak;
+    st = obrni(st, st0) * predznak;
+    return st;
+}
 
+int main() {
+    int a = dobiStevilo();
+    int b = dobiStevilo();
     int sum = a + b;
 
     izpis(sum);
