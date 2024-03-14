3e54aedb8374186ad185c666d6d6e277f784b023
Anže Pintar <anze@anzepintar.com>
2024-03-14 09:51:22 +0100

    vaje 03

---
+++ vaje/vaje03/lychrelovaStevila/lychrelova.c
@@ -0,0 +1,51 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+long obrni(long n){
+    long obrnjeno = 0;
+    while (n != 0) { 
+        long tmp = n % 10; 
+        obrnjeno = obrnjeno * 10 + tmp; 
+        n = n / 10; 
+    } 
+    return obrnjeno;
+}
+
+bool jePalindrom(long n){
+    return(n == obrni(n));
+}
+
+bool jeLychrelovo(long n, int k){
+    do {
+        n = n + obrni(n);
+        k--;
+    }
+    while (!jePalindrom(n) && n<1e17L && k>0);
+    
+    return jePalindrom(n);
+}
+
+int main() {
+ /*   long testno;
+    scanf("%d", &testno);
+
+    if(jeLychrelovo(testno)){
+        printf("ly\n");
+    }
+    else{
+        printf("ni ly\n");
+    }
+*/
+    int k, a, b;
+    scanf("%d %d %d", &k, &a, &b);
+    int steviloLySt = 0;
+    for (int i = a; i < b; i++) {
+        if(!jeLychrelovo(i, k))
+            steviloLySt++;
+    }
+    printf("%d\n", steviloLySt);
+    return 0;
+}
+
---
+++ vaje/vaje03/prijateljska.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+
+
+int vsotaPravihDeliteljev(int st){
+    int vsota = 0;
+    
+    for(int i = 1; i <= sqrt(st); i++){
+        if(st%i == 0){
+            vsota += i;
+            if(st / i != i && st / i != st){
+                vsota += st/i;
+            }
+        }
+    }
+    return vsota;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int kandidat = vsotaPravihDeliteljev(n);
+
+    if (vsotaPravihDeliteljev(kandidat) == n){
+        printf("%d\n", kandidat);
+    }
+    else {
+        printf("NIMA\n");
+    }
+    return 0;
+}
+
